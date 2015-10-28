d392f48b3c2c5c482e13aaa858ca6e6a4eaf5c1f
Julia Guenther <juliag1@alumni.stanford.edu>
2015-10-28 15:09:35 -0700

    Created data tables

--- model.py
+++ model.py
@@ -14,14 +14,6 @@ class Customers (Model.db):
     customer_phone_number = db.Column(db.String(50), nullable=True)
     customer_job_title = db.Column(db.String(50), nullable=True)
 
-
-class Tickets (Model.db):
-    
-    __tablename__ = "tickets"
-
-    ticket_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
-
 class Companies (Model.db):
     
     __tablename__ = "companies"
@@ -42,3 +34,18 @@ class Agents (Model.db):
     agent_password = db.Column(db.String(50), nullable=True)
     agent_email = db.Column(db.String(50), nullable=True)
     agent_tier = db.Column(db.String(10), nullable=True)
+
+class Tickets (Model.db):
+    
+    __tablename__ = "tickets"
+
+    ticket_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    customer_id = db.Column(db.Integer(), db.ForeignKey('customers.customer_id'), nullable=False)
+    company_id = db.Column(db.Integer(), db.ForeignKey('companies.commpany_id'), nullable=False)
+    agent_id = db.Column(db.Integer(), db.ForeignKey('agents.agent_id'), nullable=False)
+    time_submitted = db.Column(db.DateTime())
+    channel_submitted = db.Column(db.String(50), nullable=True)
+    ticket_content = db.Column(db.String(), nullable=False)
+    resolution_time = db.Column(db.Integer())
+    num_agent_touches = db.Column(db.Integer())
+    first_response_time = db.Column(db.Integer())
