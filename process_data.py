facee0cf11da353f1c4f17f9af46736c42455b01
Julia Guenther <juliag1@alumni.stanford.edu>
2015-11-02 14:08:07 -0800

    Successfully pulled ticket submission times data

--- process_data.py
+++ process_data.py
@@ -2,9 +2,16 @@ import pandas as pd
 import datetime
 import numpy as np
 
-#This line reads in an excel file and converts it to a numpy array
-data= pd.read_excel('ticket_contents.xls').info()
+from sklearn.cluster import KMeans
+#This line reads in an excel file and converts it to a pandas DataFrame
+data= pd.read_excel('ticket_contents.xls')
 
-#Get the clusters from the 
+#This line gets all of the times that tickets were submitted 
+times_submitted = data.iloc[:,[2]]
+
+# print type(data)
+#Total number of clusters = 24 * 7 = 168
+# n_clusters = 168
+
 
 
