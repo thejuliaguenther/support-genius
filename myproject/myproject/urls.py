43e355ba56d2731cdc78b4c5c167ed0557365c67
Julia Guenther <juliag1@alumni.stanford.edu>
2015-11-02 03:16:39 -0800

    Working on routes. Broken

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
+    url(r'^ticket_system/', include('ticket_system.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
