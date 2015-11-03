a208f85cab0c14895141e64a52976c6c48d43ca6
Julia Guenther <juliag1@alumni.stanford.edu>
2015-11-02 16:46:55 -0800

    Working on kmeans

--- process_data.py
+++ process_data.py
@@ -9,6 +9,17 @@ data= pd.read_excel('ticket_contents.xls')
 #This line gets all of the times that tickets were submitted 
 times_submitted = data.iloc[:,[2]]
 
+times_and_weekdays = [[]]
+
+# print type(times_submitted)
+
+submitted_arr = np.array(times_submitted)
+
+for time in submitted_arr:
+    
+
+print type(submitted_arr)
+
 # print type(data)
 #Total number of clusters = 24 * 7 = 168
 # n_clusters = 168
