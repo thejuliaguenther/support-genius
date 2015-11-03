683acb0f8aca17dcba7dc9149f12e5a3606babe7
Julia Guenther <juliag1@alumni.stanford.edu>
2015-11-02 16:58:16 -0800

    Added view of all the tickets

--- myproject/ticket_system/views.py
+++ myproject/ticket_system/views.py
@@ -6,3 +6,9 @@ def index(request):
     template = loader.get_template('ticket_system/dashboard.html')
     context = RequestContext(request)
     return HttpResponse(template.render(context))
+
+def tickets(request):
+    template = loader.get_template('ticket_system/tickets.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
+
