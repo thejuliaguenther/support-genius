2a2f5bd16a0d611491f4d61bf258e5bb03fc210b
Julia Guenther <juliag1@alumni.stanford.edu>
2015-11-07 13:36:01 -0800

    Basic individual ticket view working

--- hackbright_project/server.py
+++ hackbright_project/server.py
@@ -1,9 +1,9 @@
 from jinja2 import StrictUndefined
 from datetime import datetime
 from time import strptime
-from flask import Flask, render_template, redirect, request, flash, session
+from flask import Flask, render_template, redirect, request, flash, session, jsonify, json, url_for
 from flask_debugtoolbar import DebugToolbarExtension
-from model import Ticket, Agent, connect_to_db, db
+from model import Ticket, Agent, Customer, connect_to_db, db
 
 app = Flask(__name__)
 app.secret_key = "Hello world"
@@ -58,39 +58,53 @@ def process_login():
     
 
 @app.route('/tickets/<int:ticket_id>')
-def show_ticket_detail():
+def show_ticket_detail(ticket_id):
     """Shows individual ticket details"""
-
-    return render_template("individual_ticket.html")
+    ticket = Ticket.query.filter(Ticket.ticket_id == ticket_id)
+    selected_ticket_id = ticket_id
+    
+    return render_template("individual_ticket.html", selected_ticket_id=selected_ticket_id)
 
 @app.route('/user_detail/<int:user_id>')
 def show_user_detail():
     """Shows details about a specific customer"""
-
+    
     return render_template("user_detail.html")
 
 @app.route('/dashboard')
-def show_dashboard():
-    """Shows the ticket analytics dashboard"""
+def get_tickets_to_display():
+    """ Gets the tickets to display in the dashboard heatmap"""
     tickets = Ticket.query.order_by(Ticket.ticket_id).all()
 
-    tickets_by_time = {}
+    tickets_by_time = []
 
     for ticket in tickets:
-        ticket_submitted = strptime(ticket.time_submitted)
+        ticket_submitted = ticket.time_submitted
+        ticket_id = ticket.ticket_id
         ticket_day_of_week = ticket_submitted.weekday()
         ticket_hour_submitted = ticket_submitted.hour
         
         if ticket_hour_submitted not in tickets_by_time:
-            tickets_by_time
-        ticket_tuple = (ticket_submitted, ticket_day_of_week, ticket_hour_submitted)
-        tickets_by_time.append(ticket_tuple)
+            ticket_dict = (ticket_id, ticket_day_of_week, ticket_hour_submitted)
+            tickets_by_time.append(ticket_dict)
+    # json_tickets = jsonify(tickets_by_time=tickets_by_time)
+    json_tickets = json.dumps(tickets_by_time)
+    print type(json_tickets)
+    return render_template("dashboard.html", json_tickets=json_tickets)
+# def display_dashboard():
+#     """ Displays the graphs showing ticket metrics"""
+#     return render_template("dashboard.html")
 
-    return render_template("dashboard.html")
+    
+# @app.route('/dashboard')
+# def show_dashboard():
+#     """Shows the ticket analytics dashboard"""
+
+#     return render_template("dashboard.html")
 
 
 if __name__ == "__main__":
-    app.debug = False
+    app.debug = True
     connect_to_db(app)
 
     DebugToolbarExtension(app)
