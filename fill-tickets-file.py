4cae9fa1266b204b95dcea80c4519a7172f3d73f
Julia Guenther <juliag1@alumni.stanford.edu>
2015-12-06 16:53:29 -0800

    Cleaning up

---
+++ fill-tickets-file.py
@@ -0,0 +1,38 @@
+from random import choice 
+
+# file= open("companies.txt")
+# companies = {}
+# for line in file:
+#     company_file = file.readline()
+#     split_line = company_file.split('|')
+#     print companies
+    #companies[split_line[0]] = companies[split_line[1:]
+
+
+# for line in split_file:
+#     companies[split_file[line[0]] = split_file[line[1:]]
+
+
+
+
+# def generate_company():
+#     for i in range(300):
+#         curr_company = choice(companies.keys())
+#         print curr_company
+#         print companies[curr_company]
+
+
+customers = ["Strongplus", "Unitax", "Icecan Corp", "Ventotechno Holdings", "Indigoplex", "Geoline", 
+"X-taxon", "runhex", "tanflex", "Tampcode LLC", "zimis media", "Keyline Partners", "Qvodexon International",
+"vaiaway", "Zimplus" , "Groovelane", "Silverbam", "Plexfase", "Qvofan", "Iceit", "Cityzimin", "sunzone",
+"Villanix", "Voltcore", "Opefan", "Kontrans", "Bioquote", "Medialine"]
+
+titles = ["Account Executive", "Product Marketing Manager", "Sales Development Representative", "Associate Consultant", "Partner", "Business Development Associate", "Operations Analyst"]
+channels = ['Text', 'Twitter', 'Email', 'Other']
+
+for i in range(300):
+    print choice(range(1,41))
+    
+
+
+
